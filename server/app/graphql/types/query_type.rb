582390e63a1f34928f43e6e254b97043da14650a
Sienna <sienna.m.wood@gmail.com>
2018-05-31 14:52:00 -0600

    alphabetize and add allCollections and allRepositories

--- server/app/graphql/types/query_type.rb
+++ server/app/graphql/types/query_type.rb
@@ -1,19 +1,27 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
 
-  field :allCountries, types[Types::CountryType] do
-    resolve ->(obj, args, ctx) { Country.all }
+  field :allCatalogers, types[Types::CatalogerType] do
+    resolve ->(obj, args, ctx) { Cataloger.all }
   end
 
-  field :allMediaTypes, types[Types::MediaTypeType] do
-    resolve ->(obj, args, ctx) { MediaType.all }
+  field :allCollections, types[Types::CollectionType] do
+    resolve ->(obj, args, ctx) { Collection.all }
+  end
+
+  field :allCountries, types[Types::CountryType] do
+    resolve ->(obj, args, ctx) { Country.all }
   end
 
   field :allMaterialFormats, types[Types::MaterialFormatType] do
     resolve ->(obj, args, ctx) { MaterialFormat.all }
   end
 
-  field :allCatalogers, types[Types::CatalogerType] do
-    resolve ->(obj, args, ctx) { Cataloger.all }
+  field :allMediaTypes, types[Types::MediaTypeType] do
+    resolve ->(obj, args, ctx) { MediaType.all }
+  end
+
+  field :allRepositories, types[Types::RepositoryType] do
+    resolve ->(obj, args, ctx) { Repository.all }
   end
 end
