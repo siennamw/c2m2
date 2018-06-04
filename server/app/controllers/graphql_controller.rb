0d2e2440174c889d5a38535f56e582198f585b84
Sienna <sienna.m.wood@gmail.com>
2018-06-04 14:10:02 -0600

    fix typo in graphql controller

--- server/app/controllers/graphql_controller.rb
+++ server/app/controllers/graphql_controller.rb
@@ -7,7 +7,7 @@ class GraphqlController < ApplicationController
       session: session,
       current_user: current_user
     }
-    result = GraphqlTutorialSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    result = C2m2BackendSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
 
     render json: result
   end
