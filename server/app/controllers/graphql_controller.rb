6ff060e26c0f9a4ea4c36b60ca153e3f47caf0d3
Sienna <sienna.m.wood@gmail.com>
2018-06-06 16:09:08 -0600

    fix current_user function

--- server/app/controllers/graphql_controller.rb
+++ server/app/controllers/graphql_controller.rb
@@ -20,9 +20,9 @@ class GraphqlController < ApplicationController
     return unless session[:token]
 
     crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base.byteslice(0..31))
-    token = crypt.decrypt_and_verify session[:token]
-    user_id = token.gsub('user-id:', '').to_i
-    User.find_by id: user_id
+    token = crypt.decrypt_and_verify(session[:token])
+    user_id = token.gsub('cataloger-id:', '').to_i
+    Cataloger.find(user_id)
   rescue ActiveSupport::MessageVerifier::InvalidSignature
     nil
   end
