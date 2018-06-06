7c8ff90ff30ae68f4f6eba19d04bc37e5832fea0
Sienna <sienna.m.wood@gmail.com>
2018-06-06 15:01:35 -0600

    add remaining mutation resolvers to mutation_type

--- server/app/graphql/types/mutation_type.rb
+++ server/app/graphql/types/mutation_type.rb
@@ -1,9 +1,16 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
-  field :createCountry, function: Resolvers::CreateCountry.new
-  field :createMediaType, function: Resolvers::CreateMediaType.new
-  field :createMaterialFormat, function: Resolvers::CreateMaterialFormat.new
   field :createCataloger, function: Resolvers::CreateCataloger.new
+  field :createCollection, function: Resolvers::CreateCollection.new
+  field :createComposer, function: Resolvers::CreateComposer.new
+  field :createCountry, function: Resolvers::CreateCountry.new
+  field :createDirector, function: Resolvers::CreateDirector.new
+  field :createMaterialFormat, function: Resolvers::CreateMaterialFormat.new
+  field :createMediaType, function: Resolvers::CreateMediaType.new
+  field :createProductionCompany, function: Resolvers::CreateProductionCompany.new
+  field :createPublisher, function: Resolvers::CreatePublisher.new
+  field :createRepository, function: Resolvers::CreateRepository.new
+  field :createWork, function: Resolvers::CreateWork.new
   field :signInCataloger, function: Resolvers::SignInCataloger.new
 end
