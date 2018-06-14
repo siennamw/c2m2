2686857881604c5ee2b10839602b87a1763d8010
Sienna <sienna.m.wood@gmail.com>
2018-06-14 15:07:41 -0600

    change to less, use sienna-boilerplate

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app container">
+        <header className="row">
+          <h1>C2M2</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main className="row">
+          <p>
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </main>
       </div>
     );
   }
