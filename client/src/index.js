8b35ee2ab829472d2b591a831fce5a5b24d947a5
Sienna <sienna.m.wood@gmail.com>
2018-10-27 09:20:20 -0600

    add graphql and apollo dependencies and provider

--- client/src/index.js
+++ client/src/index.js
@@ -6,5 +6,27 @@ import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3000/graphql'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Router>
+      <App />
+    </Router>
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
