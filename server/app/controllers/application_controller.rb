435cec9103a3664af2a44ae6c0f68259642cdc1a
Sienna <sienna.m.wood@gmail.com>
2018-11-04 17:30:26 -0700

    enable CORS

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # TODO: temporary for development
+  # protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 end
