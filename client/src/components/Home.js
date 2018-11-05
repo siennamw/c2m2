b5b9c7499eba62eecb323dd0b3cd938d8625b9e4
Sienna <sienna.m.wood@gmail.com>
2018-11-04 20:59:44 -0700

    tweak to headings on homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -10,10 +10,9 @@ const Home = () => (
       You may also <Link to="/contact">contact us with general questions or
         comments</Link> or <Link to="/vision">read more about our plans for C2M2</Link>.</p>
 
-    <h3>Contributors</h3>
     <div className="row">
       <div className="six columns">
-        <h3>Michael W. Harris, Ph.D.</h3>
+        <h4>Michael W. Harris, Ph.D.</h4>
         <p>Michael is a librarian, archivist, and musicologist. C2M2 is Michael's
           brainchild. For more about
           Michael, <a href="http://www.michaelwharris.net/" target="_blank"
@@ -24,7 +23,7 @@ const Home = () => (
         </p>
       </div>
       <div className="six columns">
-        <h3>Sienna M. Wood, Ph.D.</h3>
+        <h4>Sienna M. Wood, Ph.D.</h4>
         <p>Sienna is a musicologist and software engineer. She built this website. For
           more about Sienna, <a href="http://www.siennamwood.com" target="_blank"
                                 rel="noopener noreferrer">
