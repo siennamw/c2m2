fccdaa61f72808936bafd3292977a0ed2811cecd
Sienna <sienna.m.wood@gmail.com>
2018-11-17 13:54:55 -0700

    country form

---
+++ client/src/components/forms/CountryForm.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .required('Name is required'),
+  description: Yup.string(),
+});
+
+const InnerCountryForm = ({ handleSubmit, isSubmitting }) => {
+  return (
+    <Form>
+      <label htmlFor='name'>
+        Name <ErrorMessage name='name' component='div' className='form-error' />
+      </label>
+      <Field type='text'
+             name='name'
+             className='u-full-width'/>
+      <label htmlFor='description'>
+        Description <ErrorMessage name='description' component='div' className='form-error' />
+      </label>
+      <Field type='text'
+             name='description'
+             className='u-full-width'
+             component='textarea'/>
+      <button type='submit'
+              className='button-primary u-full-width'
+              disabled={isSubmitting}
+              onClick={handleSubmit}>
+        Submit
+      </button>
+    </Form>
+  )
+};
+
+const CountryForm = () => {
+  return (
+    <div>
+      <h3>New Country</h3>
+      <Formik
+        initialValues={{
+          name: '',
+          description: '',
+        }}
+        validationSchema={validationSchema}
+        onSubmit={(values, { setSubmitting }) => console.log(values)}
+        render={InnerCountryForm}
+      />
+    </div>
+  )
+};
+
+export default CountryForm;
