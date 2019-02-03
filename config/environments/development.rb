0f414f16734913704ac02914f7f19ac037338854
Sienna <sienna.m.wood@gmail.com>
2019-02-02 19:25:36 -0700

    SendGrid configuration for emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Disable email delivery in development
+  config.action_mailer.perform_deliveries = false
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
