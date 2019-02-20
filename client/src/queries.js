b7b59522fc5a601a49b835b93154343daa1d70fb
Sienna <sienna.m.wood@gmail.com>
2019-02-19 20:37:41 -0700

    client-side query by id for all entries

--- client/src/queries.js
+++ client/src/queries.js
@@ -81,6 +81,203 @@ export const LIST_ALL_REPOSITORIES = gql`
     }
 `;
 
+export const CATALOGER_BY_ID = gql`
+  query cataloger($id: ID!){
+      cataloger(id: $id){
+          id
+          name
+          email
+          description
+          catalogers {
+              id
+              name
+          }
+          collections {
+              id
+              name
+          }
+          composers {
+              id
+              name
+          }
+          countries {
+              id
+              name
+          }
+          directors {
+              id
+              name
+          }
+          material_formats {
+              id
+              name
+          }
+          media_types {
+              id
+              name
+          }
+          production_companies {
+              id
+              name
+          }
+          publishers {
+              id
+              name
+          }
+          repositories {
+              id
+              name
+          }
+          works {
+              id
+              name
+          }
+      }
+  }
+`;
+
+export const COLLECTION_BY_ID = gql`
+    query collection($id: ID!){
+        collection(id: $id){
+            id
+            name
+            repository {
+                id
+            }
+            description
+        }
+    }
+`;
+
+export const COMPOSER_BY_ID = gql`
+    query composer($id: ID!){
+        composer(id: $id){
+            id
+            name
+            imdb_link
+        }
+    }
+`;
+
+export const COUNTRY_BY_ID = gql`
+    query country($id: ID!){
+        country(id: $id){
+            id
+            name
+            description
+        }
+    }
+`;
+
+export const DIRECTOR_BY_ID = gql`
+    query director($id: ID!){
+        director(id: $id){
+            id
+            name
+            imdb_link
+        }
+    }
+`;
+
+export const MATERIAL_FORMAT_BY_ID = gql`
+    query material_format($id: ID!){
+        material_format(id: $id){
+            id
+            name
+            description
+        }
+    }
+`;
+
+export const MEDIA_TYPE_BY_ID = gql`
+    query media_type($id: ID!){
+        media_type(id: $id){
+            id
+            name
+            description
+        }
+    }
+`;
+
+export const PRODUCTION_COMPANY_BY_ID = gql`
+    query production_company($id: ID!){
+        production_company(id: $id){
+            id
+            name
+            description
+        }
+    }
+`;
+
+export const PUBLISHER_BY_ID = gql`
+    query publisher($id: ID!){
+        publisher(id: $id){
+            id
+            name
+            description
+        }
+    }
+`;
+
+export const REPOSITORY_BY_ID = gql`
+    query repository($id: ID!){
+        repository(id: $id){
+            id
+            name
+            description
+        }
+    }
+`;
+
+export const WORK_BY_ID = gql`
+  query work($id: ID!){
+      work(id: $id){
+          id
+          title
+          secondary_title
+          alias_alternates
+          year
+          digital_copy_link
+          finding_aid_link
+          rights_holder
+          citation_source
+          cataloging_notes
+          country {
+              id
+              name
+          }
+          material_format {
+              id
+              name
+          }
+          media_type {
+              id
+              name
+          }
+          collections {
+              id
+              name
+          }
+          composers {
+              id
+              name
+          }
+          directors {
+              id
+              name
+          }
+          production_companies {
+              id
+              name
+          }
+          publishers {
+              id
+              name
+          }
+      }
+  }
+`;
+
 export const WORKS_SEARCH = gql`
     query searchWorks($filter: WorkFilter, $first: Int, $skip: Int){
         allWorks(filter: $filter, first: $first, skip: $skip) {
