398668628e6ff7e36bd0f45b5955221ebbc9a5e9
Sienna <sienna.m.wood@gmail.com>
2019-02-19 17:30:19 -0700

    define constant to convert singular to plural field name if needed

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,1 +1,8 @@
 export const LOCAL_STORAGE_KEY = 'c2m2-auth';
+
+// for field names that cannot be changed to plural with the addition of 's'
+export const FIELD_TO_PLURAL = {
+  country: 'countries',
+  production_company: 'production_companies',
+  repository: 'repositories',
+};
