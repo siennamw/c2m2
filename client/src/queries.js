8d609a75a25bd5da2a3d60a6537f396e971c43b6
Sienna <sienna.m.wood@gmail.com>
2019-02-21 21:42:30 -0700

    fix whitespace in queries

--- client/src/queries.js
+++ client/src/queries.js
@@ -1,302 +1,302 @@
 import gql from 'graphql-tag';
 
 export const LIST_ALL_COLLECTIONS = gql`
-    query allCollections {
-        allCollections {
-            id
-            name
-        }
+  query allCollections {
+    allCollections {
+      id
+      name
     }
+  }
 `;
 
 export const LIST_ALL_COMPOSERS = gql`
-    query allComposers {
-        allComposers {
-            id
-            name
-        }
+  query allComposers {
+    allComposers {
+      id
+      name
     }
+  }
 `;
 
 export const LIST_ALL_COUNTRIES = gql`
-    query allCountries {
-        allCountries {
-            id
-            name
-        }
+  query allCountries {
+    allCountries {
+      id
+      name
     }
+  }
 `;
 
 export const LIST_ALL_DIRECTORS = gql`
-    query allDirectors {
-        allDirectors {
-            id
-            name
-        }
+  query allDirectors {
+    allDirectors {
+      id
+      name
     }
+  }
 `;
 
 export const LIST_ALL_MATERIAL_FORMATS = gql`
-    query allMaterialFormats {
-        allMaterialFormats {
-            id
-            name
-        }
+  query allMaterialFormats {
+    allMaterialFormats {
+      id
+      name
     }
+  }
 `;
 
 export const LIST_ALL_MEDIA_TYPES = gql`
-    query allMediaTypes {
-        allMediaTypes {
-            id
-            name
-        }
+  query allMediaTypes {
+    allMediaTypes {
+      id
+      name
     }
+  }
 `;
 
 export const LIST_ALL_PRODUCTION_COMPANIES = gql`
-    query allProductionCompanies {
-        allProductionCompanies {
-            id
-            name
-        }
+  query allProductionCompanies {
+    allProductionCompanies {
+      id
+      name
     }
+  }
 `;
 
 export const LIST_ALL_PUBLISHERS = gql`
-    query allPublishers {
-        allPublishers {
-            id
-            name
-        }
+  query allPublishers {
+    allPublishers {
+      id
+      name
     }
+  }
 `;
 
 export const LIST_ALL_REPOSITORIES = gql`
-    query allRepositories {
-        allRepositories {
-            id
-            name
-        }
+  query allRepositories {
+    allRepositories {
+      id
+      name
     }
+  }
 `;
 
 export const CATALOGER_BY_ID = gql`
   query cataloger($id: ID!){
-      cataloger(id: $id){
-          id
-          name
-          email
-          description
-          catalogers {
-              id
-              name
-          }
-          collections {
-              id
-              name
-          }
-          composers {
-              id
-              name
-          }
-          countries {
-              id
-              name
-          }
-          directors {
-              id
-              name
-          }
-          material_formats {
-              id
-              name
-          }
-          media_types {
-              id
-              name
-          }
-          production_companies {
-              id
-              name
-          }
-          publishers {
-              id
-              name
-          }
-          repositories {
-              id
-              name
-          }
-          works {
-              id
-              name
-          }
+    cataloger(id: $id){
+      id
+      name
+      email
+      description
+      catalogers {
+        id
+        name
       }
+      collections {
+        id
+        name
+      }
+      composers {
+        id
+        name
+      }
+      countries {
+        id
+        name
+      }
+      directors {
+        id
+        name
+      }
+      material_formats {
+        id
+        name
+      }
+      media_types {
+        id
+        name
+      }
+      production_companies {
+        id
+        name
+      }
+      publishers {
+        id
+        name
+      }
+      repositories {
+        id
+        name
+      }
+      works {
+        id
+        name
+      }
+    }
   }
 `;
 
 export const COLLECTION_BY_ID = gql`
-    query collection($id: ID!){
-        collection(id: $id){
-            id
-            name
-            repository {
-                id
-            }
-            description
-        }
+  query collection($id: ID!){
+    collection(id: $id){
+      id
+      name
+      repository {
+        id
+      }
+      description
     }
+  }
 `;
 
 export const COMPOSER_BY_ID = gql`
-    query composer($id: ID!){
-        composer(id: $id){
-            id
-            name
-            imdb_link
-        }
+  query composer($id: ID!){
+    composer(id: $id){
+      id
+      name
+      imdb_link
     }
+  }
 `;
 
 export const COUNTRY_BY_ID = gql`
-    query country($id: ID!){
-        country(id: $id){
-            id
-            name
-            description
-        }
+  query country($id: ID!){
+    country(id: $id){
+      id
+      name
+      description
     }
+  }
 `;
 
 export const DIRECTOR_BY_ID = gql`
-    query director($id: ID!){
-        director(id: $id){
-            id
-            name
-            imdb_link
-        }
+  query director($id: ID!){
+    director(id: $id){
+      id
+      name
+      imdb_link
     }
+  }
 `;
 
 export const MATERIAL_FORMAT_BY_ID = gql`
-    query material_format($id: ID!){
-        material_format(id: $id){
-            id
-            name
-            description
-        }
+  query material_format($id: ID!){
+    material_format(id: $id){
+      id
+      name
+      description
     }
+  }
 `;
 
 export const MEDIA_TYPE_BY_ID = gql`
-    query media_type($id: ID!){
-        media_type(id: $id){
-            id
-            name
-            description
-        }
+  query media_type($id: ID!){
+    media_type(id: $id){
+      id
+      name
+      description
     }
+  }
 `;
 
 export const PRODUCTION_COMPANY_BY_ID = gql`
-    query production_company($id: ID!){
-        production_company(id: $id){
-            id
-            name
-            description
-        }
+  query production_company($id: ID!){
+    production_company(id: $id){
+      id
+      name
+      description
     }
+  }
 `;
 
 export const PUBLISHER_BY_ID = gql`
-    query publisher($id: ID!){
-        publisher(id: $id){
-            id
-            name
-            description
-        }
+  query publisher($id: ID!){
+    publisher(id: $id){
+      id
+      name
+      description
     }
+  }
 `;
 
 export const REPOSITORY_BY_ID = gql`
-    query repository($id: ID!){
-        repository(id: $id){
-            id
-            name
-            description
-        }
+  query repository($id: ID!){
+    repository(id: $id){
+      id
+      name
+      description
     }
+  }
 `;
 
 export const WORK_BY_ID = gql`
   query work($id: ID!){
-      work(id: $id){
-          id
-          title
-          secondary_title
-          alias_alternates
-          year
-          digital_copy_link
-          finding_aid_link
-          rights_holder
-          citation_source
-          cataloging_notes
-          country {
-              id
-              name
-          }
-          material_format {
-              id
-              name
-          }
-          media_type {
-              id
-              name
-          }
-          collections {
-              id
-              name
-          }
-          composers {
-              id
-              name
-          }
-          directors {
-              id
-              name
-          }
-          production_companies {
-              id
-              name
-          }
-          publishers {
-              id
-              name
-          }
+    work(id: $id){
+      id
+      title
+      secondary_title
+      alias_alternates
+      year
+      digital_copy_link
+      finding_aid_link
+      rights_holder
+      citation_source
+      cataloging_notes
+      country {
+        id
+        name
       }
+      material_format {
+        id
+        name
+      }
+      media_type {
+        id
+        name
+      }
+      collections {
+        id
+        name
+      }
+      composers {
+        id
+        name
+      }
+      directors {
+        id
+        name
+      }
+      production_companies {
+        id
+        name
+      }
+      publishers {
+        id
+        name
+      }
+    }
   }
 `;
 
 export const WORKS_SEARCH = gql`
-    query searchWorks($filter: WorkFilter, $first: Int, $skip: Int){
-        allWorks(filter: $filter, first: $first, skip: $skip) {
-            id
-            year
-            title
-            secondary_title
-            composers {
-                id
-                name
-            }
-            directors {
-                id
-                name
-            }
-            country {
-                id
-                name
-            }
-        }
+  query searchWorks($filter: WorkFilter, $first: Int, $skip: Int){
+    allWorks(filter: $filter, first: $first, skip: $skip) {
+      id
+      year
+      title
+      secondary_title
+      composers {
+        id
+        name
+      }
+      directors {
+        id
+        name
+      }
+      country {
+        id
+        name
+      }
     }
+  }
 `;
