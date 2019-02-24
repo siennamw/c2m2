1f5804fb50eb505f5baac9675a69257363651d80
Sienna <sienna.m.wood@gmail.com>
2019-02-24 16:52:02 -0700

    fallback rails route renders react app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-  # TODO: temporary for development
-  # protect_from_forgery with: :exception
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
+
+  def fallback_index_html
+    render :file => 'public/index.html'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   end
 
   post "/graphql", to: "graphql#execute"
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
