f6568647660fb0748e683c045ee24bb345b4c5b7
Sienna <sienna.m.wood@gmail.com>
2019-02-24 16:13:26 -0700

    consolidate selectOnChange callback in WorkForm

--- client/src/components/entries/work/WorkForm.js
+++ client/src/components/entries/work/WorkForm.js
@@ -19,25 +19,22 @@ export const WorkForm = ({
   handleSubmit, isSubmitting, isValid, status, setFieldValue,
 }) => {
   const selectOnChange = (evt, name) => {
-    // when selecting from a dropdown to set an ID,
-    // coerce value to number on change
-    if (name.includes('_id')) {
-      setFieldValue(
-        name,
-        Number(evt.target.value),
-      );
-    }
-  };
-  const multiSelectOnChange = (evt, name) => {
-    // when selecting from a multiselect to set an array of IDs,
-    // build array manually & coerce values to numbers on change
     if (name.includes('_ids')) {
+      // when selecting from a multiselect to set an array of IDs,
+      // build array manually & coerce values to numbers on change
       setFieldValue(
         name,
         // turn array-like object into a real array
         [].slice
           .call(evt.target.selectedOptions)
           .map(option => Number(option.value)),
+      );
+    } else if (name.includes('_id')) {
+      // when selecting from a dropdown to set an ID,
+      // coerce value to number on change
+      setFieldValue(
+        name,
+        Number(evt.target.value),
       );
     }
   };
@@ -145,7 +142,7 @@ export const WorkForm = ({
         displayName="Collection(s)"
         fieldName="collection_ids"
         isMulti
-        onChangeCallback={multiSelectOnChange}
+        onChangeCallback={selectOnChange}
         query={queries.LIST_ALL_COLLECTIONS}
         queryName="allCollections"
         componentForModal={<NewCollection />}
@@ -154,7 +151,7 @@ export const WorkForm = ({
         displayName="Composer(s)"
         fieldName="composer_ids"
         isMulti
-        onChangeCallback={multiSelectOnChange}
+        onChangeCallback={selectOnChange}
         query={queries.LIST_ALL_COMPOSERS}
         queryName="allComposers"
         componentForModal={<NewComposer />}
@@ -163,7 +160,7 @@ export const WorkForm = ({
         displayName="Director(s)"
         fieldName="director_ids"
         isMulti
-        onChangeCallback={multiSelectOnChange}
+        onChangeCallback={selectOnChange}
         query={queries.LIST_ALL_DIRECTORS}
         queryName="allDirectors"
         componentForModal={<NewDirector />}
@@ -172,7 +169,7 @@ export const WorkForm = ({
         displayName="Production Company or Companies"
         fieldName="production_company_ids"
         isMulti
-        onChangeCallback={multiSelectOnChange}
+        onChangeCallback={selectOnChange}
         query={queries.LIST_ALL_PRODUCTION_COMPANIES}
         queryName="allProductionCompanies"
         componentForModal={<NewProductionCompany />}
@@ -181,7 +178,7 @@ export const WorkForm = ({
         displayName="Publisher(s)"
         fieldName="publisher_ids"
         isMulti
-        onChangeCallback={multiSelectOnChange}
+        onChangeCallback={selectOnChange}
         query={queries.LIST_ALL_PUBLISHERS}
         queryName="allPublishers"
         componentForModal={<NewPublisher />}
