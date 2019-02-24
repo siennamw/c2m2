a0999c6c8495f8fedbf3c0e6826cb6422ee1b78a
Sienna <sienna.m.wood@gmail.com>
2019-02-24 12:09:48 -0700

    test that works search is case insensitive

--- test/graphql/resolvers/works_search_test.rb
+++ test/graphql/resolvers/works_search_test.rb
@@ -6,9 +6,19 @@ class Resolvers::WorksSearchTest < ActiveSupport::TestCase
   end
 
   setup do
-    @cataloger = Cataloger.create!( name: 'test', email: 'test@example.com', password: '12345678' )
-    @media_type = MediaType.create!(name: 'a media type', cataloger: @cataloger)
-    @material_format = MaterialFormat.create!(name: 'a material format', cataloger: @cataloger)
+    @cataloger = Cataloger.create!(
+      name: 'test',
+      email: 'test@example.com',
+      password: '12345678'
+    )
+    @media_type = MediaType.create!(
+      name: 'a media type',
+      cataloger: @cataloger
+    )
+    @material_format = MaterialFormat.create!(
+      name: 'a material format',
+      cataloger: @cataloger
+    )
   end
 
   test 'filter option' do
@@ -36,6 +46,36 @@ class Resolvers::WorksSearchTest < ActiveSupport::TestCase
         { 'title_contains' => 'title2' }
       ]
     }
+    )
+
+    assert_equal result.map(&:title).sort, [works[1], works[2], works[3]].map(&:title).sort
+  end
+
+  test 'filter option is case insensitive' do
+    works = []
+    4.times do |n|
+      works << Work.create!(
+        cataloger: @cataloger,
+        title: "TITLE#{n}",
+        secondary_title: "Secondary#{n}",
+        alias_alternates: "aLiAs#{n}",
+        year: 2018,
+        media_type: @media_type,
+        material_format: @material_format,
+      )
+    end
+
+    result = find(
+      filter: {
+        'title_contains' => 'title1',
+        'OR' => [
+          {
+            'secondary_title_contains' => 'secondary2',
+            'OR' => [{ 'alias_alternates_contains' => 'alias3' }]
+          },
+          { 'title_contains' => 'title2' }
+        ]
+      }
     )
 
     assert_equal result.map(&:title).sort, [works[1], works[2], works[3]].map(&:title).sort
