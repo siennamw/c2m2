e9dbf821f828ef5320133319e62a1bdea7c8eea5
Sienna <sienna.m.wood@gmail.com>
2019-05-25 15:26:06 -0600

    remove data grooming from EditEntry

--- client/src/components/entries/EditEntry.js
+++ client/src/components/entries/EditEntry.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Query } from 'react-apollo';
 
-import { FIELD_TO_PLURAL } from '../../constants';
 import NewEntry from './NewEntry';
 
 const EditEntry = ({
@@ -31,38 +30,12 @@ const EditEntry = ({
         const { selfIsAdmin } = data;
         const entryIsSelf = data[queryName].is_self;
 
-        // extract ids from objects to populate form fields
-        let k;
-        const groomedData = Object.keys(yupSchema.fields).reduce((acc, key) => {
-          if (key.includes('_ids')) {
-            // extract array of ids from array of objects
-            k = key.split('_ids')[0];
-            k = FIELD_TO_PLURAL[k] ? FIELD_TO_PLURAL[k] : `${k}s`;
-
-            acc[key] = data[queryName][k].reduce((a, i) => {
-              a.push(i.id);
-              return a;
-            }, []);
-          } else if (key.includes('_id')) {
-            // extract id from object
-            k = key.split('_id')[0];
-            acc[key] = data[queryName][k] && data[queryName][k].id
-              ? data[queryName][k].id
-              : undefined;
-          } else {
-            acc[key] = data[queryName][key] || '';
-          }
-          return acc;
-        }, {});
-
-        const initialValues = yupSchema.cast(groomedData);
-
         content = (
           <NewEntry
             FormComponent={FormComponent}
             entryIsSelf={entryIsSelf}
             gqlMutation={gqlMutation}
-            initialValues={initialValues}
+            initialValues={data[queryName]}
             selfIsAdmin={selfIsAdmin}
             title={title}
             yupSchema={yupSchema}
