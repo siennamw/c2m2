08026fee02b521325e16e7efda91ab583ff07d16
Sienna <sienna.m.wood@gmail.com>
2019-05-25 15:59:07 -0600

    clear unnecessary code from NewWork

--- client/src/components/entries/work/NewWork.js
+++ client/src/components/entries/work/NewWork.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import NewEntry from '../NewEntry';
 
@@ -7,34 +6,14 @@ import WorkForm from './WorkForm';
 import { CREATE_WORK } from '../../../mutations';
 import { workValidationSchema } from '../../../validationSchemas';
 
-const NewWork = ({ initialValues, title }) => (
+const NewWork = () => (
   <NewEntry
     clearAfterSubmit
     title="New Work"
     gqlMutation={CREATE_WORK}
-    initialValues={initialValues}
     yupSchema={workValidationSchema}
     FormComponent={WorkForm}
   />
 );
-
-NewWork.defaultProps = {
-  initialValues: null,
-  title: 'New Work',
-};
-
-NewWork.propTypes = {
-  initialValues: (props, propName, componentName) => {
-    workValidationSchema
-      .isValid(props[propName])
-      .then((valid) => {
-        if (!valid) {
-          return new Error(`Invalid prop ${propName} supplied to ${componentName}.`);
-        }
-        return true;
-      });
-  },
-  title: PropTypes.string,
-};
 
 export default NewWork;
