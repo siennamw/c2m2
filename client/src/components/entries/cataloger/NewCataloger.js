36bdae7721457be2f3b25761adbec3a27b18a09d
Sienna <sienna.m.wood@gmail.com>
2019-06-26 08:34:11 -0600

    add error message when non-admin attempts to create a cataloger

--- client/src/components/entries/cataloger/NewCataloger.js
+++ client/src/components/entries/cataloger/NewCataloger.js
@@ -21,17 +21,25 @@ const NewCataloger = () => (
             Sorry! There was an error fetching data.
           </div>
         );
-      } else if (data && data.selfIsAdmin) {
-        content = (
-          <NewEntry
-            clearAfterSubmit
-            FormComponent={CatalogerForm}
-            gqlMutation={CREATE_CATALOGER}
-            selfIsAdmin={data.selfIsAdmin}
-            title="New Cataloger"
-            yupSchema={catalogerValidationSchema}
-          />
-        );
+      } else if (data) {
+        if (data.selfIsAdmin) {
+          content = (
+            <NewEntry
+              clearAfterSubmit
+              FormComponent={CatalogerForm}
+              gqlMutation={CREATE_CATALOGER}
+              selfIsAdmin={data.selfIsAdmin}
+              title="New Cataloger"
+              yupSchema={catalogerValidationSchema}
+            />
+          );
+        } else {
+          content = (
+            <div className="status-message error">
+              Sorry! Only administrators can create new catalogers.
+            </div>
+          );
+        }
       }
 
       return content;
