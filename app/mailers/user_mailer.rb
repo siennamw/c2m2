658295627c9f18fa79aebb7f6320832ce4da797e
Sienna <sienna.m.wood@gmail.com>
2019-09-10 07:49:19 -0600

    password reset emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,10 @@ class UserMailer < ApplicationMailer
     @url  = 'https://c2m2-dev.herokuapp.com/sign-in'
     mail(to: @user.email, subject: 'Welcome to C2M2 Dev')
   end
+
+  def reset_password_token_email(user, token)
+    @user = user
+    @url  = "https://c2m2-dev.herokuapp.com/reset-password/#{token}"
+    mail(to: @user.email, subject: 'Password Reset for C2M2 Dev')
+  end
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -10,4 +10,15 @@ class UserMailerPreview < ActionMailer::Preview
 
     UserMailer.welcome_email(cataloger)
   end
+
+  def reset_password_token_email
+    # http://localhost:3000/rails/mailers/user_mailer/reset_password_token_email.html
+    cataloger = Cataloger.first
+
+    unless cataloger
+      cataloger = { email: 'example@preview.com' }
+    end
+
+    UserMailer.reset_password_token_email(cataloger, 'token0123')
+  end
 end
