9a307087fd0ac9d2558aca13125595e09d793de1
Sienna <sienna.m.wood@gmail.com>
2019-12-18 08:54:26 -0700

    update welcome email to direct to password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,13 @@
 class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
-    @url  = 'https://c2m2-dev.herokuapp.com/sign-in'
+    @url = 'https://c2m2-dev.herokuapp.com/request-reset-password'
     mail(to: @user.email, subject: 'Welcome to C2M2 Dev')
   end
 
   def reset_password_token_email(user, token)
     @user = user
-    @url  = "https://c2m2-dev.herokuapp.com/reset-password/#{token}"
+    @url = "https://c2m2-dev.herokuapp.com/reset-password/#{token}"
     mail(to: @user.email, subject: 'Password Reset for C2M2 Dev')
   end
 end
