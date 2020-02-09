29628441cbc2ac9dc97839b30a8f4cb6a78d5bdb
Sienna <sienna.m.wood@gmail.com>
2020-02-09 14:34:24 -0700

    hide bulk upload from non-admin catalogers

--- client/src/components/catalogers/CatalogerDashboard.js
+++ client/src/components/catalogers/CatalogerDashboard.js
@@ -1,36 +1,52 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import { NavLink, Route, Switch } from 'react-router-dom';
+
+import { AuthContext } from '../AuthContext';
 
 import Nav from '../nav/Nav';
 
+import BulkUpload from './BulkUpload';
 import CatalogerEditAccount from './CatalogerEditAccount';
 import CatalogerHome from './CatalogerHome';
 import CatalogerNewEntryParent from './CatalogerNewEntryParent';
 import CatalogerEditEntryParent from './CatalogerEditEntryParent';
 
-import BulkUpload from './BulkUpload';
-
 import UnknownRoute from '../UnknownRoute';
 
-const CatalogerDashboard = ({ match }) => (
-  <div>
-    <h2>Cataloger Dashboard</h2>
-    <Nav ariaLabel="Cataloger dashboard">
-      <NavLink to={`${match.path}/home`}>Home</NavLink>
-      <NavLink to={`${match.path}/new`}>New Entry</NavLink>
-      <NavLink to={`${match.path}/bulk-upload`}>Bulk Upload</NavLink>
-      <NavLink to={`${match.path}/account`}>Account</NavLink>
-      <NavLink to="/sign-out">Sign Out</NavLink>
-    </Nav>
-    <Switch>
-      <Route path={`${match.path}/account`} component={CatalogerEditAccount} />
-      <Route path={`${match.path}/edit`} component={CatalogerEditEntryParent} />
-      <Route path={`${match.path}/home`} component={CatalogerHome} />
-      <Route path={`${match.path}/new`} component={CatalogerNewEntryParent} />
-      <Route path={`${match.path}/bulk-upload`} component={BulkUpload} />
-      <Route component={UnknownRoute} />
-    </Switch>
-  </div>
-);
+const CatalogerDashboard = ({ match }) => {
+  const { admin } = useContext(AuthContext);
+
+  return (
+    <div>
+      <h2>Cataloger Dashboard</h2>
+      <Nav ariaLabel="Cataloger dashboard">
+        <NavLink to={`${match.path}/home`}>Home</NavLink>
+        <NavLink to={`${match.path}/new`}>New Entry</NavLink>
+        {
+          admin
+            ? <NavLink to={`${match.path}/bulk-upload`}>Bulk Upload</NavLink>
+            : null
+        }
+        <NavLink to={`${match.path}/account`}>Account</NavLink>
+        <NavLink to="/sign-out">Sign Out</NavLink>
+      </Nav>
+      <Switch>
+        <Route path={`${match.path}/account`} component={CatalogerEditAccount} />
+        <Route path={`${match.path}/bulk-upload`} component={BulkUpload} />
+        <Route path={`${match.path}/edit`} component={CatalogerEditEntryParent} />
+        <Route path={`${match.path}/home`} component={CatalogerHome} />
+        <Route path={`${match.path}/new`} component={CatalogerNewEntryParent} />
+        <Route component={UnknownRoute} />
+      </Switch>
+    </div>
+  );
+};
+
+CatalogerDashboard.propTypes = {
+  match: PropTypes.shape({
+    path: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default CatalogerDashboard;
