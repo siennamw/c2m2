f88c218eb2bd3bca6b1156b0c62512475ea58503
Sienna <sienna.m.wood@gmail.com>
2020-02-13 08:44:14 -0700

    cataloger directory for dashboard

---
+++ client/src/components/catalogers/CatalogerDirectory.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+import CatalogersList from '../entries/cataloger/CatalogersList';
+
+const CatalogerDirectory = () => (
+  <Fragment>
+    <h3>Cataloger Directory</h3>
+    <CatalogersList />
+  </Fragment>
+);
+
+export default CatalogerDirectory;
---
+++ client/src/components/entries/cataloger/CatalogersList.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+
+import { LIST_ALL_CATALOGERS } from '../../../queries';
+import { wrapWithLink } from '../../../utils';
+
+const CatalogersList = () => {
+  const {
+    data,
+    error,
+    loading,
+  } = useQuery(LIST_ALL_CATALOGERS);
+
+  if (loading) {
+    return (
+      <div className="status-message">
+        Fetching...
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="status-message error">
+        Sorry! There was an error fetching results.
+      </div>
+    );
+  }
+
+  const haveData = data && data.allCatalogers && data.allCatalogers.length > 0;
+
+  if (haveData) {
+    return (
+      <table className="u-full-width">
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Admin</th>
+          </tr>
+          {
+            data.allCatalogers.map(cataloger => (
+              <tr>
+                <td>
+                  {wrapWithLink(cataloger.name, cataloger.id, 'cataloger')}
+                </td>
+                <td>
+                  <a href={`mailto:${cataloger.email}`}>
+                    {cataloger.email}
+                  </a>
+                </td>
+                <td>
+                  {cataloger.admin ? 'Yes' : 'No'}
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    );
+  }
+
+  return (
+    <div className="status-message error">
+      Sorry! No results were found.
+    </div>
+  );
+};
+
+export default CatalogersList;
