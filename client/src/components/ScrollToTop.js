90188378ffaf281b46cf4f943412517c0f062e64
Sienna <sienna.m.wood@gmail.com>
2020-09-22 08:28:33 -0600

    eslint disabled for window reference in ScrollToTop

--- client/src/components/ScrollToTop.js
+++ client/src/components/ScrollToTop.js
@@ -5,7 +5,7 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo(0, 0); // eslint-disable-line no-undef
   }, [pathname]);
 
   return null;
