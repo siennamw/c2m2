b57870c586baa07d7d8345cdf189383e7d21dc4a
Sienna <sienna.m.wood@gmail.com>
2021-01-12 09:08:02 -0700

    update contributors list on home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -21,48 +21,56 @@ const Home = () => (
       .
     </p>
 
-    <div className="row">
-      <div className="six columns">
-        <h2 className="h4">
-          Michael W. Harris, Ph.D.
-        </h2>
-        <p>
-          {'Michael is a librarian, archivist, and musicologist. C2M2 is Michael\'s brainchild. For more about Michael, '}
-          <a
-            href="http://www.michaelwharris.net/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            visit his website
-          </a>
-          {' or '}
-          <a
-            href="http://www.thetemptrack.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            his blog
-          </a>
-          .
-        </p>
-      </div>
-      <div className="six columns">
-        <h2 className="h4">
-          Sienna M. Wood, Ph.D.
-        </h2>
-        <p>
-          {'Sienna is a musicologist and software engineer. She built this website. For more about Sienna, '}
-          <a
-            href="http://www.siennamwood.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            visit her website
-          </a>
-          .
-        </p>
-      </div>
-    </div>
+    <h2>Contributors</h2>
+    <dt>Michael W. Harris, Ph.D.</dt>
+    <dd>
+      {'Michael is a librarian, archivist, and musicologist. C2M2 is Michael\'s brainchild. For more about Michael, '}
+      <a
+        href="http://www.michaelwharris.net/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        visit his website
+      </a>
+      {' or '}
+      <a
+        href="http://www.thetemptrack.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        his blog
+      </a>
+      .
+    </dd>
+
+    <dt>Sienna M. Wood, Ph.D.</dt>
+    <dd>
+      {'Sienna is a musicologist and software engineer. She built this website. For more about Sienna, '}
+      <a
+        href="http://www.siennamwood.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        visit her website
+      </a>
+      .
+    </dd>
+
+    <dt>Heather Fisher</dt>
+    <dd>
+      {'Heather is the Metadata/Catalog Librarian at Saginaw Valley State University. She holds degrees in music performance, musicology (with an emphasis on film, television, and radio music from 1930-1960), and library and information science. Her thesis studied Max Steiner’s score for '}
+      <i>Gone with the Wind</i>
+      , particularly the influence of traditional music on the film’s narrative.
+    </dd>
+
+    <dt>Joshua A. Henry, M.M.</dt>
+    <dd>
+      Joshua is a music cataloger. As a member of the Film Music Interest Group
+      of the Music Library Association, Josh has been assisting with data
+      structure and input for C2M2.
+    </dd>
+
+    {/*<dt>Jeff Lyon</dt>*/}
   </div>
 );
 
