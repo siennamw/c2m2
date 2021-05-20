8923e0a93dcfc6278b4614ea57e7816a25d8f304
Sienna <sienna.m.wood@gmail.com>
2021-05-19 18:47:52 -0600

    migration to enable UUIDs in DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_212106) do
+ActiveRecord::Schema.define(version: 2021_05_20_003054) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
