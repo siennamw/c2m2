419bba2d1b88bbdf2ed1ff34395f51bbaf4c8de9
Sienna <sienna.m.wood@gmail.com>
2021-05-27 11:34:47 -0600

    update generator config for UUIDs as default

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,4 +1,4 @@
 Rails.application.config.generators do |g|
   # by default, primary keys are UUIDs
-  g.orm :active_record, primary_key_type: :uuid
+  g.orm :active_record, primary_key_type: :uuid, generate_method: 'gen_random_uuid()'
 end
