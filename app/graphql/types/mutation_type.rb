cf25bacd580b47e2b6fd377e925e94e5faaa3be1
Sienna <sienna.m.wood@gmail.com>
2021-05-29 19:46:14 -0600

    sort mutation types

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -6,12 +6,12 @@ Types::MutationType = GraphQL::ObjectType.define do
   field :createComposer, function: Resolvers::CreateComposer.new
   field :createCountry, function: Resolvers::CreateCountry.new
   field :createDirector, function: Resolvers::CreateDirector.new
-  field :createWork, function: Resolvers::CreateWork.new
   field :createMaterialFormat, function: Resolvers::CreateMaterialFormat.new
   field :createMediaType, function: Resolvers::CreateMediaType.new
   field :createProductionCompany, function: Resolvers::CreateProductionCompany.new
   field :createRepository, function: Resolvers::CreateRepository.new
   field :createResource, function: Resolvers::CreateResource.new
+  field :createWork, function: Resolvers::CreateWork.new
 
   field :deleteCollection, function: Resolvers::DeleteCollection.new
   field :deleteComposer, function: Resolvers::DeleteComposer.new
@@ -30,16 +30,16 @@ Types::MutationType = GraphQL::ObjectType.define do
   field :updateComposer, function: Resolvers::UpdateComposer.new
   field :updateCountry, function: Resolvers::UpdateCountry.new
   field :updateDirector, function: Resolvers::UpdateDirector.new
-  field :updateWork, function: Resolvers::UpdateWork.new
   field :updateMaterialFormat, function: Resolvers::UpdateMaterialFormat.new
   field :updateMediaType, function: Resolvers::UpdateMediaType.new
   field :updateProductionCompany, function: Resolvers::UpdateProductionCompany.new
   field :updateRepository, function: Resolvers::UpdateRepository.new
   field :updateResource, function: Resolvers::UpdateResource.new
+  field :updateWork, function: Resolvers::UpdateWork.new
 
-  field :signInCataloger, function: Resolvers::SignInCataloger.new
   field :getResetPasswordToken, function: Resolvers::GetResetPasswordToken.new
   field :resetPassword, function: Resolvers::ResetPassword.new
+  field :signInCataloger, function: Resolvers::SignInCataloger.new
 
   field :handleContactForm, function: Resolvers::HandleContactForm.new
   field :handleSuggestionForm, function: Resolvers::HandleSuggestionForm.new
