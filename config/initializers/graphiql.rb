f2e566ea0b94da344f8fb8c4cd90276b3620efc0
Sienna <sienna.m.wood@gmail.com>
2021-06-12 19:13:25 -0600

    update auth header for graphiql in development

--- config/initializers/graphiql.rb
+++ config/initializers/graphiql.rb
@@ -1,5 +1,6 @@
 if Rails.env.development?
-  GraphiQL::Rails.config.headers['Authorization'] = -> (_ctx) {
-    "bearer #{ENV['JWT_TOKEN']}"
+  GraphiQL::Rails.config.headers['Authorization'] = ->(_ctx) {
+    # need an admin cataloger in DB to create a token for GraphiQL
+    "Bearer #{AuthToken.token(Cataloger.find_by(admin: true))}"
   }
 end
