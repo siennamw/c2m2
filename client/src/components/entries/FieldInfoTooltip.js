296d3fcbff8e9b9c8d37e6ead56086bfbf0ecce8
Sienna <sienna.m.wood@gmail.com>
2021-06-12 21:13:12 -0600

    create Tooltip component; refactor FieldInfoTooltip

--- client/src/components/entries/FieldInfoTooltip.js
+++ client/src/components/entries/FieldInfoTooltip.js
@@ -1,27 +1,11 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+
+import Tooltip from '../Tooltip';
 
 import { MODEL_NAMES, TOOLTIP_BY_MODEL_AND_FIELD } from '../../constants';
 
 const FieldInfoTooltip = ({ field, model }) => {
-  const [show, setShow] = useState(false);
-
-  const showDisplay = () => {
-    setShow(true);
-  };
-
-  const hideDisplay = () => {
-    setShow(false);
-  };
-
-  const handleTouch = () => {
-    if (show) {
-      hideDisplay();
-    } else {
-      showDisplay();
-    }
-  };
-
   const tooltip = TOOLTIP_BY_MODEL_AND_FIELD(model, field);
 
   const rules = tooltip && tooltip.rules && tooltip.rules.length > 0
@@ -34,35 +18,23 @@ const FieldInfoTooltip = ({ field, model }) => {
     ? tooltip.semantics
     : 'No description available for this field.';
 
-  const display = (
-    <div className="tooltip-content">
-      <div className="tooltip-description">{semantics}</div>
-      {
-        rules
-          ? (
-            <Fragment>
-              <hr />
-              <p>Rules</p>
-              <ul className="tooltip-rules">{rules}</ul>
-            </Fragment>
-          )
-          : undefined
-      }
-    </div>
-  );
-
   return (
-    <span
-      className="field-info-tooltip"
-      onBlur={hideDisplay}
-      onFocus={showDisplay}
-      onMouseLeave={hideDisplay}
-      onMouseOver={showDisplay}
-      onTouchStart={handleTouch}
-    >
-      <div className="tooltip-anchor">&#8505;</div>
-      {show ? display : ''}
-    </span>
+    <Tooltip>
+      <>
+        <div className="tooltip-description">{semantics}</div>
+        {
+          rules
+            ? (
+              <Fragment>
+                <hr />
+                <p>Rules</p>
+                <ul className="tooltip-rules">{rules}</ul>
+              </Fragment>
+            )
+            : undefined
+        }
+      </>
+    </Tooltip>
   );
 };
 
