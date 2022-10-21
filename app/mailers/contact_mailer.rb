918ad31e456e353f765c91744e7f14ed711c8993
Sienna <sienna.m.wood@gmail.com>
2022-10-20 18:10:31 -0600

    fix from and reply_to values in contact_mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,17 +1,17 @@
 class ContactMailer < ApplicationMailer
-  default to: ENV['GMAIL_USERNAME']
+  default to: ENV['GMAIL_USERNAME'], from: ENV['GMAIL_USERNAME']
 
   def contact_email(name, email, message)
     @name = name
     @email = email
     @message = message
-    mail(from: @email, subject: 'Contact Form Submission from C2M2')
+    mail(reply_to: @email, subject: 'Contact Form Submission from C2M2')
   end
 
   def suggestion_email(name, email, fields)
     @name = name
     @email = email
     @fields = fields
-    mail(from: @email, subject: 'Suggestion Form Submission from C2M2')
+    mail(reply_to: @email, subject: 'Suggestion Form Submission from C2M2')
   end
 end
