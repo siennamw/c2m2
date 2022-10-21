1d5be68465f53f4daf3d5d17d15cbef97cde4404
Sienna <sienna.m.wood@gmail.com>
2022-10-20 21:00:33 -0600

    email deliveries enabled temporarily for testing

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,5 +3,6 @@ require_relative './production'
 Rails.application.configure do
   # Disable email delivery in staging
   # (alternatively, could use https://mailtrap.io/)
-  config.action_mailer.perform_deliveries = false
+  # TODO: deliveries enabled temporarily for testing; reset to false when done
+  config.action_mailer.perform_deliveries = true
 end
